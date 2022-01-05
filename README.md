2346b0b010251c7f8e4ed0d4682d9d1cd26a376e
bom4672 <kansha755@gmail.com>
2022-01-06 05:17:37 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-rgn2z7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-rgn2z7)
---
+++ index.js
@@ -0,0 +1,37 @@
+// Import stylesheets
+import "./style.css"
+
+// 0. Import LIFF SDK
+
+// Body element
+const Body = document.getElementById("Body")
+
+// Profile elements
+const pictureUrl = document.getElementById("pictureUrl")
+const userID = document.getElementById("userID")
+const displayName = document.getElementById("displayName")
+const statusMessage = document.getElementById("statusMessage")
+const email = document.getElementById("email")
+
+// Button elements
+const btnShare = document.getElementById("btnShare")
+
+
+async Function main() {
+  // 2. liff.ready
+  // 3. Try a LIFF Function
+  // 4. Check where LIFF was opened
+  // 5. Call getUserProfile()
+  // 10. Show share Button
+
+  // 1. Initialize LIFF app)
+}
+main();
+
+// 6. Create getUserProfile()
+// *7. Get email
+
+// *8. Create shareMsg()
+// 11. Add close window
+
+// 9. Add event listener to share Button
